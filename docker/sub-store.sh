6f1ae2fd47f25cd140798d71db625ccc9c3673da
sunchaowang <sunchaowang@outlook.com>
2023-07-09 23:03:47 +0800

    init

--- docker/sub-store.sh
+++ docker/sub-store.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-gitPath="/git"
-rootPath="/Sub-Store"
-backend="$rootPath/backend"
+gitPath="/"
+rootPath="/root/Sub-Store"
+backend="$rootPath/Back"
 web="$rootPath/Front"
 
 echo -e "======================== 1、更 新 仓 库 ========================\n"
 
-    cd "$gitPath/Front" && git reset --hard && git pull 
+    cd "$rootPath/Front" && git reset --hard && git pull 
     sleep 2s
-    cd "$gitPath/Sub-Store" && git reset --hard && git pull
+    cd "$rootPath/Back" && git reset --hard && git pull
     sleep 2s
-    cd "$gitPath/Docker" && git reset --hard && git pull
+    cd "$rootPath/Docker" && git reset --hard && git pull
     sleep 2s
-    ln -sf "$gitPath/Docker/docker/sub-update.sh" /usr/bin/sub_update && chmod +x /usr/bin/sub_update
+    # ln -sf "$gitPath/Docker/docker/sub-update.sh" /usr/bin/sub_update && chmod +x /usr/bin/sub_update
 
 echo -e "==============================================================\n"
 
@@ -29,14 +29,14 @@ echo -e "==============================================================\n"
 
 echo -e "======================== 3、启动后端接口 ========================\n"
 
-    cp -r "$gitPath/Sub-Store/backend" "$rootPath"
+    # cp -r "$rootPath/Back" "$rootPath"
     cd "$backend" && pnpm install && pnpm build
     pm2 start sub-store.min.js --name "sub-store" --source-map-support --time
 
 echo -e "==============================================================\n"
 
 echo -e "======================== 4、启动 Sub-Store 界面 ========================\n"
-    cp -r "$gitPath/Front" "$rootPath"
+    # cp -r "$rootPath/Front" "$rootPath"
     cd "$web"
 
     echo -e "删除自带后端地址，追加配置环境变量配置的后端地址\n"
